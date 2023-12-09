d9372ddebb48c14454ba40e13ca4a8a8e233a3af
gi0v1 <giovipegoraro@gmail.com>
2023-12-09 18:49:31 +0100

    First commit

--- save_video.py
+++ save_video.py
@@ -7,23 +7,26 @@ from picamera2 import Picamera2
 cv2.startWindowThread()
     
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (1920, 1080)}))
+config = picam2.create_preview_configuration(lores={"size": (640, 480)})
+picam2.configure(config)
 picam2.start()
 
    
 # Below VideoWriter object will create 
 # a frame of above defined The output  
 # is stored in 'filename.avi' file. 
-result = cv2.VideoWriter('/saved_vids/picam3.avi',  
+result = cv2.VideoWriter('picam3.avi',  
                          cv2.VideoWriter_fourcc(*'MJPG'), 
-                         10, (1920, 1080))
+                         10, (640, 480))
     
 while(True): 
     #frame = cv2.cvtColor(picam2.capture_array(), cv2.COLOR_RGB2RGB)
-    frame = picam2.capture_array()
+    #frame = picam2.capture_array()
 
 
-  
+    frame = picam2.capture_array("lores")
+    frame = cv2.cvtColor(frame, cv2.COLOR_YUV420p2RGB)
+
     # Write the frame into the 
     # file 'filename.avi' 
     result.write(frame) 
